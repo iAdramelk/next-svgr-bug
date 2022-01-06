c2ac956fbd72a20bd7949afeaa889dac30db6004
Alexey Ivanov <mail@alexeyivanov.info>
2022-01-06 23:45:54 +0900

    test case

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      oneOf: [
+        {
+          issuer: /\.[mjt]sx?$/,
+          resourceQuery: /react/,
+          use: "@svgr/webpack",
+        },
+        {
+          type: "asset/resource",
+        },
+      ],
+    });
+
+    return config;
+  }
 }
