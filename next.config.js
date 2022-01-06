8c77b0605223fc76b13a3d01b71adc1b012f4be7
Alexey Ivanov <mail@alexeyivanov.info>
2022-01-07 00:22:12 +0900

    Add some options

--- next.config.js
+++ next.config.js
@@ -6,6 +6,13 @@ module.exports = {
     domains: ["i.ytimg.com"],
   },
   webpack: (config) => {
+    config.output.assetModuleFilename = "static/images/[hash][ext]";
+
+    config.module.rules.push({
+      test: /\.(png|jpg|webp|gif|mp4|webm|ogg|swf|ogv|woff2)$/i,
+      type: "asset/resource",
+    });
+
     config.module.rules.push({
       test: /\.svg$/,
       oneOf: [
